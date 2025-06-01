2a1b32067347f724d00ef8145cdb4aa1ce9a3f72
Fkhrayef <alkhrayefff@gmail.com>
2025-06-02 02:58:40 +0300

    format landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
         width={519}
         height={472}
       />
+
       <div>
         <Logo />
         <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
@@ -20,16 +21,16 @@ export default function Home() {
         </h1>
         <p className="text-2xl font-medium max-w-[600px]">
           Use PetSoft to easily keep track of pets under your care. Get lifetime
-          access for $299
+          access for $299.
         </p>
-      </div>
-      <div className="mt-10 space-x-3">
-        <Button asChild>
-          <Link href="/signup">Get Started</Link>
-        </Button>
-        <Button asChild variant="secondary">
-          <Link href="/login">Log in</Link>
-        </Button>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get started</Link>
+          </Button>
+          <Button asChild variant="secondary">
+            <Link href="/login">Log in</Link>
+          </Button>
+        </div>
       </div>
     </main>
   );
